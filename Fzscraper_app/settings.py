f6829ce3a9c937dcb5149b3228f308c784e5481e
Chiazor-Daniel <chiazordaniel317@gmail.com>
2024-09-26 12:44:41 +0100

    update

--- Fzscraper_app/settings.py
+++ Fzscraper_app/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
